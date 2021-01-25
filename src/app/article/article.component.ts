a3f4e624bfaead6ea8442c077e1fed3b223ce024
Marenda S.A <renda_ardy@outlook.com>
2021-01-25 19:58:05 +0800

    chore: initial commit

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,23 @@
+import { Component, HostBinding, Input } from '@angular/core';
+
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent {
+  @HostBinding('attr.class') cssClass = 'row';
+  @Input() article?: Article;
+
+  voteUp(): boolean {
+    this.article?.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article?.voteDown();
+    return false;
+  }
+}
